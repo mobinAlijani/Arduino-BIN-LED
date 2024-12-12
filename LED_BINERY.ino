98f92aa00af3803ad05e73d18e7ed5e165acd668
Matin_alijani <matin.alijani@gmail.com>
2024-12-11 21:23:37 -0800

    Update LED_BINERY.ino
    Use bitRead function instead of bit-shifting .
    Declared LEDpin as const .

--- LED_BINERY.ino
+++ LED_BINERY.ino
@@ -1,22 +1,19 @@
-uint8_t x;
+uint8_t x; 
 
-int LEDpin[8]={11,9,3,18,16,7,5,4};
+const int LEDpin[8] = {11, 9, 3, 18, 16, 7, 5, 4};
 
 void setup() {
   Serial.begin(115200);
-  for(int i=0;i<8;i++){
-    pinMode(LEDpin[i],OUTPUT);
+  for (int i = 0; i < 8; i++) {
+    pinMode(LEDpin[i], OUTPUT);
   }
 }
 
-void loop(){
-
-  if(Serial.available()){
-    x =Serial.parseInt();
-    for(int i=0;i<8;i++){
-      byte y= pow(2,i);
-      digitalWrite(LEDpin[i],(x&y)>>i);
+void loop() {
+  if (Serial.available()) {
+    x = Serial.parseInt();
+    for (int i = 0; i < 8; i++) {
+      digitalWrite(LEDpin[i], bitRead(x, i));
     }
   }
-
 }
